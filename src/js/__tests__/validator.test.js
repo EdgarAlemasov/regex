48e7ba59c5c5650235e4e8b266db9119c1d9d61c
Edgar Alemasov <edgar.alemasov@gmail.com>
2023-02-01 21:17:48 +0500

    code refactoring

--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -1,61 +1,55 @@
 import Validator from "../validator";
 
-test ("end symbols Error", () => {
-    const name = new Validator("Trevor222");
-    expect(() => {name.validateUserName()}).toThrow("Name is mustn't end with this symbols. Please, try again!");
+test ("valid name", () => {
+    const name = "easyRave";
+    const result = Validator.validateUserName(name);
+    expect(result).toBe(true)
 });
 
-test ("start symbols Error", () => {
-    const name = new Validator("-jasper");
-    expect(() => {name.validateUserName()}).toThrow("Name is mustn't start with this symbols. Please, try again!");
+test ("end symbols", () => {
+    const name = "Trevor222"
+    const result = Validator.validateUserName(name);
+    expect(result).toBe(false)
+});
+
+test ("start symbols", () => {
+    const name = "-jasper";
+    const result = Validator.validateUserName(name);
+    expect(result).toBe(false)
 });
 
 test ("invalid symbols", () => {
-    const name = new Validator("quick*mage");
-    expect(() => {name.validateUserName()}).toThrow("You used invalid symbols. Please, try again!");
+    const name = "quick*mage";
+    const result = Validator.validateUserName(name);
+    expect(result).toBe(false)
 });
 
 test ("too much numbers", () => {
-    const name = new Validator("empty7777log");
-    expect(() => {name.validateUserName()}).toThrow("Too much numbers in a row. Please, try again!");
+    const name = "empty7777log";
+    const result = Validator.validateUserName(name);
+    expect(result).toBe(false)
 });
 
 test ("change number 1", () => {
-    const number = new Validator("Jankins", "8 (927) 000-00-00");
-    number.validatePhoneNumber();
-    const result = {
-        name: "Jankins",
-        phone: "+79270000000"
-    };
-    expect(number).toEqual(result);
+    const number = "8 (927) 000-00-00";
+    const result = "+79270000000";
+    expect(Validator.changingPhoneNumber(number)).toEqual(result);
 });
 
 test ("change number 2", () => {
-    const number = new Validator("Jankins", "+7 960 000 00 00");
-    number.validatePhoneNumber();
-    const result = {
-        name: "Jankins",
-        phone: "+79600000000"
-    };
-    expect(number).toEqual(result);
+    const number = "+7 960 000 00 00";
+    const result = "+79600000000";
+    expect(Validator.changingPhoneNumber(number)).toEqual(result);
 });
 
 test ("change number 3", () => {
-    const number = new Validator("Jankins", "+86 000 000 0000");
-    number.validatePhoneNumber();
-    const result = {
-        name: "Jankins",
-        phone: "+860000000000"
-    };
-    expect(number).toEqual(result);
+    const number = "+86 000 000 0000";
+    const result = "+860000000000";
+    expect(Validator.changingPhoneNumber(number)).toEqual(result);
 });
 
 test ("change number 4", () => {
-    const number = new Validator("Jankins", "932 (000)-0000");
-    number.validatePhoneNumber();
-    const result = {
-        name: "Jankins",
-        phone: "+79320000000"
-    };
-    expect(number).toEqual(result);
+    const number = "932 (000)-0000";
+    const result = "+79320000000";
+    expect(Validator.changingPhoneNumber(number)).toEqual(result);
 });
--- src/js/validator.js
+++ src/js/validator.js
@@ -1,37 +1,30 @@
 export default class Validator {
-    constructor (name=null, phone=null) {
-        this.name = name;
-        this.phone = phone;
-    }
-
-    validateUserName () {
+    static validateUserName (name) {
         const rightSymbols = /^[_-\w\d]+$/;
         const numbers = /\d{4}/;
         const startStr = /^[_-\d]/;
         const endStr = /[_-\d]$/;
 
-        if (!rightSymbols.test(this.name)) {
-            throw new Error ("You used invalid symbols. Please, try again!");
-        } else if (numbers.test(this.name)) {
-            throw new Error ("Too much numbers in a row. Please, try again!");
-        } else if (startStr.test(this.name)) {
-            throw new Error ("Name is mustn't start with this symbols. Please, try again!");
-        } else if (endStr.test(this.name)) {
-            throw new Error ("Name is mustn't end with this symbols. Please, try again!");
+        if (!rightSymbols.test(name) || numbers.test(name) || startStr.test(name) || endStr.test(name)) {
+            return false
+        } else {
+            return true
         }
     }
 
-    validatePhoneNumber () {
-        const findNumbers = /\D+/g
-        const concatenateNumbers = this.phone.replace(findNumbers, '')
+    static changingPhoneNumber (phone) {
+        const findNumbers = /\D+/g;
+        const concatenateNumbers = phone.replace(findNumbers, '');
+        let changedPhoneNumber;
 
         if (concatenateNumbers.length === 11) {
             const changeNum = "+" + concatenateNumbers.replace(/^8/, "7");
-            this.phone = changeNum
+            changedPhoneNumber = changeNum
         } else if (concatenateNumbers.length === 10) {
-            this.phone = "+7" + concatenateNumbers
+            changedPhoneNumber = "+7" + concatenateNumbers
         } else {
-            this.phone = "+" + concatenateNumbers
+            changedPhoneNumber = "+" + concatenateNumbers
         }
+        return changedPhoneNumber;
     }
 }
